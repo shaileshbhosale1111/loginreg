c1260bba3f8190b4552c71dc50d742be6e7cc44a
shaileshbhosale1111 <shaileshbhosale699@gmail.com>
2025-06-11 15:26:59 +0530

    Add files via upload

---
+++ Login_Ragistration/src/main/java/com/jsp/login/LoginRagistrationApplication.java
@@ -0,0 +1,13 @@
+package com.jsp.login;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoginRagistrationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LoginRagistrationApplication.class, args);
+	}
+
+}
---
+++ Login_Ragistration/src/test/java/com/jsp/login/LoginRagistrationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jsp.login;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoginRagistrationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
